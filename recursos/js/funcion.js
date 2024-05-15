a457dc250f35bf5c8649a89627acb23d23a3e9f8
alancj <anto211970@gmail.com>
2024-05-15 12:17:39 -0600

    blog

---
+++ recursos/js/funcion.js
@@ -0,0 +1,11 @@
+ // Función para mostrar la alerta al cargar la página
+ window.onload = function() {
+    // Muestra la alerta al cargar la página
+    alert('BIENVENIDOS A MI BLOG-ALAN');
+    // Muestra la página cuando se haga clic en la alerta
+    document.body.style.display = 'block';
+  };
+
+
+
+  
